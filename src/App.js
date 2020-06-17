901fd25c29ebe717e2ce3c6d632cc8f0e897894d
Jona <jonathanalvarez809@gmail.com>
2020-06-17 01:21:55 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+import { Home } from './pages/home'
+import { Detail } from './pages/detail'
+import { NotFound } from './pages/notFound'
+
+import './App.css';
+import 'bulma/css/bulma.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/detail/:movieId' component={Detail} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
